de00d85e441485c36df7f83e11d14316bb74c9dd
Manu R <manu.r@vaisesika.local>
2025-01-31 17:32:51 +0530

    Search View and corrected the CRUD options

--- user_profile/views.py
+++ user_profile/views.py
@@ -3,32 +3,54 @@ from rest_framework.views import APIView
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework.exceptions import PermissionDenied
 
 from .models import Profile
 from .serializers import ProfileSerializer
 
 
 # Create your views here.
-class ProfileCreateListView(ListCreateAPIView):
+class ProfileCRUDView(APIView):
     permission_classes = [IsAuthenticated]
-    serializer_class = ProfileSerializer
 
-    def get_queryset(self):
-        return Profile.objects.filter(user=self.request.user)
+    def get(self, request):
+        user = request.user
+        try:
+            profile = Profile.objects.get(user=user)
+            serializer = ProfileSerializer(profile)
+            return Response(serializer.data)
+        except Profile.DoesNotExist:
+            return Response({"message": "Profile not found"}, status=status.HTTP_404_NOT_FOUND)
 
-    def perform_create(self, serializer):
-        serializer.save(user=self.request.user)
+    def post(self, request):
+        user = request.user
+        serializer = ProfileSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save(user=user)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+    def put(self, request):
+        user = request.user
+        try:
+            profile = Profile.objects.get(user=user)
+            serializer = ProfileSerializer(profile, data=request.data)
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        except Profile.DoesNotExist:
+            return Response({"message": "Profile not found"}, status=status.HTTP_404_NOT_FOUND)
 
+    def patch(self, request):
+        user = request.user
+        try:
+            profile = Profile.objects.get(user=user)
+            serializer = ProfileSerializer(profile, data=request.data, partial=True)
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        except Profile.DoesNotExist:
+            return Response({"message": "Profile not found"}, status=status.HTTP_404_NOT_FOUND)
 
-class ProfileRetrieveUpdateView(RetrieveUpdateDestroyAPIView):
-    permission_classes = [IsAuthenticated]
-    serializer_class = ProfileSerializer
-
-    def get_queryset(self):
-        return Profile.objects.filter(user=self.request.user)
-
-    def perform_update(self, serializer):
-        serializer.save(user=self.request.user)
-
-        
