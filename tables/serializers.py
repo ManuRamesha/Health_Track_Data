3482b62c72f957b24c6a14095b94d15c650fdf33
Manu R <manu.r@vaisesika.local>
2025-02-14 15:57:25 +0530

    tables

--- tables/serializers.py
+++ tables/serializers.py
@@ -1,9 +1,15 @@
-
 # these are rest_framework imports
 from rest_framework import serializers
 
 # these are the local imports
 from .models import Role, Gender, Heamophilia
+
+"""
+These serializers are used to serialize the data from Role, Gender and Heamophilia models.
+They define the fields that should be included in the serializer data.
+The Meta class specifies the model and the fields to be included in the serialized data.
+The serializers are used in the views to convert the model instances to JSON data and vice versa.
+"""
 
 class RoleSerializer(serializers.ModelSerializer):
     class Meta:
